47c6cc775d2c9f4ac5d753b7b5941b62598965b6
liam <liam.bracken@gmail.com>
2020-05-27 18:50:09 +0100

    Implement responsive design for mobile

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Hamburger from "../SideDrawer/Hamburger/Hamburger";
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
-      <div>LOGO</div>
-      <nav>...</nav>
+      <Hamburger clicked={props.openDrawer} />
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav className={classes.DesktopOnly}>
+        <NavigationItems />
+      </nav>
     </header>
   );
 };
