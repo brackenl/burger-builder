ffeae2e8a9f7583c537dfca7141f1f89fe84c03f
William Bracken <liam.bracken@gmail.com>
2020-05-27 12:00:07 +0100

    Added burger components and toolbar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
+const layout = (props) => {
+  return (
+    <Aux>
+      <Toolbar />
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main className={classes.content}>{props.children}</main>
+    </Aux>
+  );
+};
+
+export default layout;
